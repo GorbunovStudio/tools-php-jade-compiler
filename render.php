51818ca947461ef09787a1e38837010c9266a68e
Kurnosov Anton <mail@kurnosov.info>
2016-10-12 16:25:02 +0300

    Create directory for a output file if it doesn't exist

--- render.php
+++ render.php
@@ -11,4 +11,10 @@ require_once("autoload.php");
 $_jade = new \Jade\Jade(true);
 $result = $_jade->render($argv[1]);
 
+if (!is_dir(dirname($argv[2]))) {
+    if (!mkdir(dirname($argv[2]), 0755, true)) {
+        exit(1);
+    }
+}
+
 file_put_contents($argv[2], $result);
